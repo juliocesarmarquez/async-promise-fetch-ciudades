56c7aa735b153b4e5c541966145ee21fbf91f6fe
Julio  Marquez <juliocesarmarquez88@gmail.com>
2021-08-11 15:36:27 -0300

    No se repite ciudad

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const fetch = require("node-fetch");
 let ciudades = ["Tokio", "Nueva York", "Los Angeles", "Paris", "Seul", "Londres", "Moscu", "Osaka", "Shanghai", "Chicago"];
-
+let seleccionados = [];
 function main() {
 
     for (let i = 0; i < 3; i++) {
         let ciudad = ciudades[Math.floor(Math.random() * ciudades.length)];
+    if (seleccionados.indexOf(ciudad) !=-1){continue;} else {seleccionados.push(ciudad);}
+    
+       
         let url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&APPID=1b5557afd2195d9941b3546d37cd1cc1&units=metric`;
         fetch(url)
             .then(response => response.json())
