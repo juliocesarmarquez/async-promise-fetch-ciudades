137e0561c89e3bf3eaa3622ef3caa503cac0e3aa
Julio  Marquez <juliocesarmarquez88@gmail.com>
2021-08-11 01:28:41 -0300

    ejercicio finalizado

---
+++ app.js
@@ -0,0 +1,21 @@
+const fetch = require("node-fetch");
+let ciudades = ["Tokio", "Nueva York", "Los Angeles", "Paris", "Seul", "Londres", "Moscu", "Osaka", "Shanghai", "Chicago"];
+
+function main() {
+
+    for (let i = 0; i < 3; i++) {
+        let ciudad = ciudades[Math.floor(Math.random() * ciudades.length)];
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&APPID=1b5557afd2195d9941b3546d37cd1cc1&units=metric`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(ciudad)
+                console.log(data.main.feels_like)
+            })
+            .catch(error => {
+                console.log("Ha ocurrido un error")
+                console.log(error)
+            })
+    }
+}
+main();
